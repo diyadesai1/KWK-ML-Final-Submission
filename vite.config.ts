69c55115d479cecbb3cd6da892de1f5c6635d624
Diya <desaivdiya@gmail.com>
2025-12-01 02:36:16 -0800

    updates to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/KWK-ML-Final-Submission/', // adjust if your GitHub repo name differs
   plugins: [react()],
 })
